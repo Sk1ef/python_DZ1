af6e88e826fad20c85a7a619c96aeed3024e0501
Sk1ef <133880154+Sk1ef@users.noreply.github.com>
2026-07-28 16:41:54 +0300

    дз2

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from Обработка_исключений import result
+while True:
+    try:
+        num1 = float(input("Введите первое число: "))
+        num2 = float(input("Введите второе число: "))
+    except ValueError:
+        print("Ошибка: Введено не число")
+        continue
 
-num1 = float(input ("Введите первое число: "))
-num2 = float(input ("Введите второе число: "))
-# operation = input("Выберите операцию")
-def summa (num1, num2):
-    result = num1 + num2
-    return result
-print(summa(num1, num2))
+    operation = input("Выберите операцию: +, -, /, *:\n")
+    correct_operations = ["+", "-", "/", "*"]
+    if operation not in correct_operations:
+        print('Неизвестная операция')
+        continue
+
+    if operation == "+":
+        print("Результат сложения: ", num1 + num2)
+    elif operation == "-":
+        print("Результат вычитания: ", num1 - num2)
+    elif operation == "*":
+        print("Результат умножения: ", num1 * num2)
+    elif operation == "/":
+        try:
+            print("Результат деления: ", num1 / num2)
+        except ZeroDivisionError:
+            print("Ошибка: Деление на ноль")
+
+    answer = input("Продолжить работу? (да/нет): ").lower()
+    if answer != "да":
+        print("Программа завершена.")
+        break
