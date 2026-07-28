6af8911a7fde0f14d603f706cad4c53c3c079be5
Sk1ef <133880154+Sk1ef@users.noreply.github.com>
2026-07-28 12:37:19 +0300

    дз2

---
+++ main.py
@@ -0,0 +1,9 @@
+from Обработка_исключений import result
+
+num1 = float(input ("Введите первое число: "))
+num2 = float(input ("Введите второе число: "))
+# operation = input("Выберите операцию")
+def summa (num1, num2):
+    result = num1 + num2
+    return result
+print(summa(num1, num2))
