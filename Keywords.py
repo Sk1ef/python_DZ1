2874d28dbd4c51a33c248dc572ad0d5093df8d94
Sk1ef <133880154+Sk1ef@users.noreply.github.com>
2026-08-02 10:11:12 +0300

    Дз

---
+++ Keywords.py
@@ -0,0 +1,22 @@
+def max_number(a, b):
+    if a >= b:
+        return a
+    else:
+        return b
+
+def empty_function():
+    pass
+
+def even_numbers(n):
+    for i in range(0, n+1):
+        if i % 2 == 0:
+            yield i
+
+assert max_number(5, 3) == 5, "Error"
+assert max_number(10, 10) == 10, "Error"
+assert max_number(3, 2) == 3, "Error"
+assert max_number(4, 8) == 8, "Error"
+print("Все тесты пройдены", "\n")
+
+for num in even_numbers(10):
+    print(num)
