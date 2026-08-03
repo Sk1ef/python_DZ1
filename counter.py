4ae8b9be3c1b3fbfea743a27b00e31ccb87d2ede
Sk1ef <133880154+Sk1ef@users.noreply.github.com>
2026-08-03 22:58:36 +0300

    counter

--- counter.py
+++ counter.py
@@ -1,9 +1,16 @@
-num = int(input("Введите число: "))
-
 def count_num(num):
-    assert num > 0, "Ошибка: Нужно ввести положительное число"
+    if num < 0:
+        print("Ошибка: Нужно ввести положительное число")
+        return
     while num >= 0:
         print(num)
         num -= 1
+
+
+try:
+    num = int(input("Введите число: "))
+except ValueError:
+    print("Нужно ввести число")
+    exit()
 
 count_num(num)
