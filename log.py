b36f079fd9137508f122ce7e0fa5cb60884e510b
Sk1ef <133880154+Sk1ef@users.noreply.github.com>
2026-08-03 22:45:40 +0300

    log

--- log.py
+++ log.py
@@ -1,3 +1,7 @@
+def check(age, citizen, disqual):
+    return age >= 18 and citizen.lower() == "да" and disqual.lower() == "нет"
+
+
 try:
     age = int(input("Введите ваш возраст: "))
 except ValueError:
@@ -7,27 +11,7 @@ except ValueError:
 citizen = input("Являетесь ли вы гражданином страны? (да/нет) ")
 disqual = input("Имеются ли обстоятельства, не допускающие вас к выборам? (да/нет) ")
 
-def check_age(age):
-    if age >= 18:
-         return True
-    else:
-        return False
-
-def check_citizen(citizen):
-    citizen = citizen.lower()
-    if citizen == "да":
-        return True
-    else:
-        return False
-
-def check_disqual(disqual):
-    disqual = disqual.lower()
-    if disqual == "да":
-        return False
-    else:
-        return True
-
-if check_citizen(citizen) and check_age(age) and check_disqual(disqual) == True:
+if check(age, citizen, disqual):
     print("Допущен")
 else:
     print("Не допущен")
