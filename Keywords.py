463997b247f88c460c9ec3976378754af0d85d69
Sk1ef <133880154+Sk1ef@users.noreply.github.com>
2026-08-04 14:30:41 +0300

    key

--- Keywords.py
+++ Keywords.py
@@ -19,11 +19,14 @@ def even_numbers(n):
             yield i
 
 
-assert max_number(5, 3) == 5, "Error"
-assert max_number(10, 10) == 10, "Error"
-assert max_number(3, 2) == 3, "Error"
-assert max_number(4, 8) == 8, "Error"
-print("Все тесты пройдены", "\n")
+def auto_test():
+    assert max_number(5, 3) == 5, "Error"
+    assert max_number(10, 10) == 10, "Error"
+    assert max_number(3, 2) == 3, "Error"
+    assert max_number(4, 8) == 8, "Error"
+    print("Все тесты пройдены", "\n")
 
+
+auto_test()
 for num in even_numbers(10):
     print(num)
