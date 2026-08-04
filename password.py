18f8ad93a2f7c4f07a0dae42eeede69404562b82
Sk1ef <133880154+Sk1ef@users.noreply.github.com>
2026-08-04 14:03:55 +0300

    pas

--- password.py
+++ password.py
@@ -5,6 +5,5 @@ while cur_password != password:
     password = input("Введите пароль: ")
     if cur_password == password:
         print("Пароль верный")
-        break
     else:
         print("Пароль не верный")
