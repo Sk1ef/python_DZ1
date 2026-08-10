be597fa9c2fd2180384ca16bf41aff0813fa3b85
Sk1ef <133880154+Sk1ef@users.noreply.github.com>
2026-08-10 17:23:19 +0300

    for

--- forwhile.py
+++ forwhile.py
@@ -1,8 +1,9 @@
-def summa():
+def sum_even_numbers():
     result = 0
-    for i in range(1, 101):
-        if i % 2 == 0:
-            result += i
+
+    for i in range(2, 101, 2):
+        result += i
+
     return result
 
 
@@ -12,18 +13,19 @@ def squares():
 
 def count_user_input():
     count = 0
-    while True:
-        number = float(input("Введите число: "))
-        if number < 0:
-            break
+    number = float(input("Введите число: "))
+
+    while number >= 0:
         count += 1
+        number = float(input("Введите число: "))
+
     return count
 
 
 def main():
-    print(f"Сумма чётных чисел от 1 до 100: {summa()}")
+    print(f"Сумма чётных чисел от 1 до 100: {sum_even_numbers()}")
     print(f"Квадраты нечётных чисел от 1 до 10: {squares()}")
-    print(f"Количество введенных неотрицательных чисел: {count_user_input()}")
+    print(f"Количество введенных чисел: {count_user_input()}")
 
 
 main()
