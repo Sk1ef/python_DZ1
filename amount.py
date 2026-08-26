33e412014d7a5b111fb1b6454d9f07621215f352
Sk1ef <133880154+Sk1ef@users.noreply.github.com>
2026-08-27 00:17:18 +0300

    amount

--- amount.py
+++ amount.py
@@ -4,12 +4,17 @@ def create_new_list(first_list, second_list):
     for i in range(min(len(first_list), len(second_list))):
         new_list.append(first_list[i] + second_list[i])
 
+    if len(first_list) > len(second_list):
+        new_list.extend(first_list[len(second_list):])
+    else:
+        new_list.extend(second_list[len(first_list):])
+
     return new_list
 
 
 def main():
-    first_list = [1, 2, 3, 4, 5]
-    second_list = [6, 7, 8, 9, 10]
+    first_list = [1, 2, 3]
+    second_list = [1, 2, 3, 4, 5]
 
     print(create_new_list(first_list, second_list))
 
