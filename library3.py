a2823a8ba766b9d9ba1c982a1b1a2392da8c9dcc
Sk1ef <133880154+Sk1ef@users.noreply.github.com>
2026-09-06 19:58:08 +0300

    libr3

---
+++ library3.py
@@ -0,0 +1,60 @@
+def book_list_view(library):
+    if not library:
+        print("Книг нет")
+    else:
+        for key in library:
+            print(key)
+
+
+def add_book(title, author, year):
+    if title in library:
+        answer = input("Такая книга уже существует. Обновить информацию? (да/нет): ")
+        if answer.lower() == "да":
+            print(f'Информация о книге "{title}" успешно обновлена.')
+        else:
+            print("Информация не была изменена.")
+            return
+    else:
+        print(f'Книга "{title}" успешно добавлена.')
+
+    library[title] = {
+        "автор": author,
+        "год": year,
+        "наличие": library[title]["наличие"] if title in library else None
+    }
+
+
+def remove_book(title):
+    if title in library:
+        del library[title]
+        print(f'Книга "{title}" успешно удалена')
+    else:
+        print("Книга не найдена")
+
+
+library = {
+    "Гарри Поттер и философский камень": {
+        "автор": "Дж. К. Роулинг",
+        "год": 1997,
+        "наличие": "в наличии"
+    },
+    "Война и мир": {
+        "автор": "Лев Толстой",
+        "год": 1869,
+        "наличие": "выдана"
+    },
+    "1984": {
+        "автор": "Джордж Оруэлл",
+        "год": 1949,
+        "наличие": "в наличии"
+    },
+    "Мастер и Маргарита": {
+        "автор": "Михаил Булгаков",
+        "год": 1967,
+        "наличие": "в наличии"
+    }
+}
+
+remove_book("Мастер и Маргарита")
+add_book("Преступление и наказание", "Фёдор Достоевский", 1866)
+book_list_view(library)
